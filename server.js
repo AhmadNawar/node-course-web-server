6eb308e6a4ac0b4ca88ac6267c205cee4508cf49
AhmadNawar <AhmadNawar@users.noreply.github.com>
2019-01-27 16:31:26 +0300

    Projects page added

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 })
+
 app.get('/', (req, res) => {
     res.render('index.hbs', {
         pageTitle: 'Home Page',
@@ -45,13 +46,20 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
     });
-})
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
 
 app.get('/bad', (req, res) => {
     res.send({
         error: 'Error handling request'
     });
 });
+
 app.listen(port, () => {
     console.log('server is upt on port ' + port);
 });
